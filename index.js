36707f468182ded4a5bc0d882b083756b4dd9c3e
Bill Burke <billstron@gmail.com>
2014-12-08 16:10:01 -0500

    adding example

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var cp = require("child_process");
-var config = require("./config.json");
-console.log("config:", config);
 
-var app = express();
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(function(req, res){
-	var type = req.get("X-GitHub-Event");
-	if(type == "push"){
-		console.log("push");
-		console.log("repo:", req.body.ref);
-		if(req.body.ref == "refs/heads/" + config.branch && req.body.repository.full_name == config.repo){
-			console.log("executing");	
-			cp.execFile(config.script, function(err, stdout, stderr){
-				if(err){
-					console.error(err);
+module.exports = function(config){
+
+	var app = express();
+	app.use(bodyParser.json({limit: '50mb'}));
+	
+	app.use(function(req, res){
+		var type = req.get("X-GitHub-Event");
+		if(type == "push"){
+			console.log("push repo, branch:", req.body.repository.full_name, req.body.ref);
+			
+			var event, out = "";
+			for(var i = 0; i < config.events.length; i++){
+				event = config.events[i];
+				if(req.body.ref == "refs/heads/" + event.branch && req.body.repository.full_name == event.repo){
+					console.log("executing");	
+					cp.execFile(config.script, function(err, stdout, stderr){
+						if(err){
+							console.error(err);
+						}
+						out += "stdout:\n\n" + stdout;
+						out += "\nstderr:\n\n" + stderr + "\n\n";
+					});	
 				}
-				res.status(200).end(stdout); 
-			});	
-		}
-		else{
+			}
+			if(out.length > 0){
+				res.status(200).end(out); 
+			}else{
+				res.status(202).end();
+			}
+		}else{
 			res.status(202).end();
 		}
-	}else{
-		res.status(202).end();
-	}
-});
+	});
 
-app.listen(8123); 
+	app.listen(config.port || 8123); 
+};
 
